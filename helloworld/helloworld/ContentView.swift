78599e83e2f4a6bdb64056cb9787e36ba6236d21
hwanseung2 <wazs555@hufs.ac.kr>
2021-03-16 09:16:58 +0900

    helloworld

---
+++ helloworld/helloworld/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  helloworld
+//
+//  Created by 유환승 on 2021/03/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world! My name is Hwanseung!!!")
+        Text("이거 이렇게 하는거 맞지?ㅎㅎㅎ")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ helloworld/helloworld/helloworldApp.swift
@@ -0,0 +1,17 @@
+//
+//  helloworldApp.swift
+//  helloworld
+//
+//  Created by 유환승 on 2021/03/16.
+//
+
+import SwiftUI
+
+@main
+struct helloworldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
