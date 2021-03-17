fbd32cf1add2abfc546802ca6db6f280c303affa
hwanseung2 <wazs555@hufs.ac.kr>
2021-03-17 12:44:33 +0900

    swift

---
+++ n_helloworld/n_helloworld/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  n_helloworld
+//
+//  Created by 유환승 on 2021/03/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    // click event
+    
+    @IBAction func Click_Move(_ sender: Any) {
+        print("Button is Clicked")
+    }
+    
+    @IBAction func ON_OFF(_ sender: Any) {
+        print("On Off Button is Clicked")
+    }
+}
+
---
+++ new_helloworld/new_helloworld/new_helloworldApp.swift
@@ -0,0 +1,17 @@
+//
+//  new_helloworldApp.swift
+//  new_helloworld
+//
+//  Created by 유환승 on 2021/03/17.
+//
+
+import SwiftUI
+
+@main
+struct new_helloworldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
