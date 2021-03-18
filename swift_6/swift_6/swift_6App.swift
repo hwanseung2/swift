94c940cfef8ea190b8e4e32e9058396459fcbacb
hwanseung2 <wazs555@hufs.ac.kr>
2021-03-18 21:54:20 +0900

    change

---
+++ swift_6/swift_6/swift_6App.swift
@@ -0,0 +1,17 @@
+//
+//  swift_6App.swift
+//  swift_6
+//
+//  Created by 유환승 on 2021/03/18.
+//
+
+import SwiftUI
+
+@main
+struct swift_6App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
--- webviewer_2/webviewer_2/Detail.swift
+++ webviewer_2/webviewer_2/Detail.swift
@@ -14,4 +14,5 @@ class Detail : UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    
 }
